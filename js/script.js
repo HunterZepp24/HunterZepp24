a37c11f05717565c12f027c1b1acfe3ed654aebd
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-02-26 22:26:43 -0800

    Experimenting With GSAP Animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("My Awesome Website");
+
+/*gsap.from(".appear",{
+    scrollTrigger: {
+      trigger: "#start",
+      start: "top 50%",
+      end: "center 50%",
+      scrub: 1
+    },
+    opacity: 0,
+    duration: 1
+  });*/
